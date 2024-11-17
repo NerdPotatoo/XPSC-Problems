ad6bd6021f1c24427d04041f1c8059ff9d459cf9
Yasir Adnan <134677437+NerdPotatoo@users.noreply.github.com>
2024-11-17 22:39:14 +0600

    Added Week4 Day1 Codes

---
+++ Week4/Day1/A_Two_Elevators.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+#define ll long long
+#define llu long long unsigned
+#define ld long double
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define fi first
+#define se second
+#define fastIO ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+using namespace std;
+const int mod = 1e9+7;
+
+void solve()
+{
+    // CODE HERE
+    int a, b, c;
+    cin >> a >> b >> c;
+    b = abs(b-c) + c;
+
+    if(a<b) cout<<1<<endl;
+    else if(b<a) cout<<2<<endl;
+    else cout<<3<<endl;
+}
+int32_t main()
+{
+    fastIO;
+    int t = 1;
+    cin>>t;
+    for(int cs = 1; cs<=t; cs++)
+    {
+        // cout<<"Case "<<cs<< ": "<<solve();
+        solve();
+    }
+    return 0;
+}
