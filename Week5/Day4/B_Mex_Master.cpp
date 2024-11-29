b96bee5462dfbc3c44f66901cd8e50984bb95f3b
Yasir Adnan <134677437+NerdPotatoo@users.noreply.github.com>
2024-11-29 23:06:38 +0600

    Added Week5 Day4 Codes

---
+++ Week5/Day4/B_Mex_Master.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+
+#define ll long long
+#define llu long long unsigned
+#define ld long double
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define fi first
+#define se second
+#define fastIO ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+using namespace std;
+const int mod = 1e9+7;
+
+void solve()
+{
+    // CODE HERE
+    int n, x;
+    cin >> n;
+    int one = 0, zero = 0, num = 0;
+    for(int i = 1; i<=n; i++)
+    {
+        cin >> x;
+        if(x==0)
+        {
+            zero++;
+        }
+        else if(x==1)
+        {
+            one++;
+        }
+        else{
+            num++;
+        }
+    }
+    if(ceil((1.0*n)/2)>=zero)
+    {
+        cout<<0<<endl;
+    }
+    else if(num>=1 || (zero == n))
+    {
+        cout<<1<<endl;
+    }
+    else{
+        cout<<2<<endl;
+    }
+
+
+}
+int32_t main()
+{
+    fastIO;
+    int t = 1;
+    cin>>t;
+    for(int cs = 1; cs<=t; cs++)
+    {
+        // cout<<"Case "<<cs<< ": "<<solve();
+        solve();
+    }
+    return 0;
+}
