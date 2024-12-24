0075d0f274c93c70813249bb73f5427834f3f773
Yasir Adnan <134677437+NerdPotatoo@users.noreply.github.com>
2024-12-24 21:18:17 +0600

    Added week7 day2 codes

---
+++ Week7/Day2/Dull_Operation.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define llu long long unsigned
+#define ld long double
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define fi first
+#define se second
+#define fastIO ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+using namespace std;
+const int mod = 1e9+7;
+
+void solve()
+{
+    // CODE HERE
+    int n;
+    cin >> n;
+    cout << n << " " << n-1 << "\n";
+}
+int32_t main()
+{
+    fastIO;
+    int t = 1;
+    cin>>t;
+    for(int cs = 1; cs<=t; cs++)
+    {
+        // cout<<"Case "<<cs<< ": "<<solve();
+        solve();
+    }
+    return 0;
+}
