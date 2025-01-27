bb08c8c5acddde79828885a9964513b2381e8461
Yasir Adnan <134677437+NerdPotatoo@users.noreply.github.com>
2025-01-27 23:24:28 +0600

    Added Week7 Day7 Code

---
+++ Week7/Day7/A_Absolute_Maximization.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define fastIO ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+const int mod = 1e9+7;
+
+void solve()
+{
+    // CODE HERE
+    int n;
+    cin >> n;
+    int x, mx = 0, mn ;
+    for(int i = 0;i<n; i++)
+    {
+        cin >> x;
+        mx |= x;
+        if(i==0) mn = x;
+        else mn &= x;
+    }
+
+    cout << mx - mn << "\n";
+}
+int32_t main()
+{
+    fastIO;
+    int T = 1;
+    cin>>T;
+    for(int cs = 1; cs <= T; cs++)
+    {
+        // cout<<"Case "<<cs<< ": "<<solve();
+        solve();
+    }
+    return 0;
+}
